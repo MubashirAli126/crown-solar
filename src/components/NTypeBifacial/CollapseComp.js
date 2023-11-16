963984c57fc116e96469a48de5cf09dfb2980edb
MubashirAli126 <48995533+MubashirAli126@users.noreply.github.com>
2023-11-16 10:20:34 +0500

    useEffects add

--- src/components/NTypeBifacial/CollapseComp.js
+++ src/components/NTypeBifacial/CollapseComp.js
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { TECollapse, TERipple } from "tw-elements-react";
 import ProductList from '@/components/NTypeBifacial/ProductList'
 import ProductDetail from '@/components/NTypeBifacial/ProductDetail'
--- src/components/NTypeMono/CollapseComp.js
+++ src/components/NTypeMono/CollapseComp.js
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { TECollapse, TERipple } from "tw-elements-react";
 import ProductList from '@/components/NTypeMono/ProductList'
 import ProductDetail from '@/components/NTypeMono/ProductDetail'
